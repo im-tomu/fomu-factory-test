8403cb5303c63e6e48efa559fad38f609d00adc1
Sean Cross <sean@xobs.io>
2019-09-16 15:31:50 +0800

    validate-spi: allow gd spi devices
    This is a stuff option, so allow these devices to be produced.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- jig/bin/validate-spi.sh
+++ jig/bin/validate-spi.sh
@@ -2,40 +2,72 @@
 
 infofile=/tmp/spi-info.txt
 rm -f $infofile
-./fomu-flash -t 4 -i | tee $infofile
+./fomu-flash -i | tee $infofile
 
 # Manufacturer ID: Macronix (c2)
 # Memory model: MX25R1635F (28)
 # Memory size: 16 Mbit (15)
 # Device ID: 15
+check_mx25r16() {
+	local infofile="$1"
+	local error_count=0
+	if ! grep -q 'Manufacturer ID: Macronix (c2)' $infofile
+	then
+		error_count=$(($error_count+1))
+	fi
 
-error_count=0
-if ! grep -q 'Manufacturer ID: Macronix (c2)' $infofile
+	if ! grep -q 'Memory model: MX25R1635F (28)' $infofile
+	then
+		error_count=$(($error_count+1))
+	fi
+
+	if ! grep -q 'Memory size: 16 Mbit (15)' $infofile
+	then
+		error_count=$(($error_count+1))
+	fi
+
+	if ! grep -q 'Device ID: 15' $infofile
+	then
+		error_count=$(($error_count+1))
+	fi
+	return $error_count
+}
+
+# Manufacturer ID: Giga Device (c8)
+# Memory model: GD25Q16C (40)
+# Memory size: 16 Mbit (15)
+# Device ID: 14
+check_gd25q16() {
+	local infofile="$1"
+	local error_count=0
+	if ! grep -q 'Manufacturer ID: Giga Device (c8)' $infofile
+	then
+		error_count=$(($error_count+1))
+	fi
+
+	if ! grep -q 'Memory model: GD25Q16C (40)' $infofile
+	then
+		error_count=$(($error_count+1))
+	fi
+
+	if ! grep -q 'Memory size: 16 Mbit (15)' $infofile
+	then
+		error_count=$(($error_count+1))
+	fi
+
+	if ! grep -q 'Device ID: 14' $infofile
+	then
+		error_count=$(($error_count+1))
+	fi
+	return $error_count
+}
+
+if ! check_mx25r16 $infofile
 then
-	echo -n "Unrecognized SPI manufacturer: "
-	grep 'Manufacturer ID: ' $infofile
-	error_count=$(($error_count+1))
+	if ! check_gd25q16 $infofile
+	then
+		echo "!!! Unrecognized SPI flash" 1>&2
+		exit 1
+	fi
 fi
-
-if ! grep -q 'Memory model: MX25R1635F (28)' $infofile
-then
-	echo -n "Unrecognized memory model: "
-	grep 'Memory model: ' $infofile
-	error_count=$(($error_count+1))
-fi
-
-if ! grep -q 'Memory size: 16 Mbit (15)' $infofile
-then
-	echo -n "Unrecognized memory size: "
-	grep 'Memory size: ' $infofile
-	error_count=$(($error_count+1))
-fi
-
-if ! grep -q 'Device ID: 15' $infofile
-then
-	echo -n "Unrecognized device id: "
-	grep 'Device ID: ' $infofile
-	error_count=$(($error_count+1))
-fi
-
-exit $error_count
+exit 0
